3b947a9e4c81ba1fa1b9a2ad23dbb7f0762236e2
Natã Nogueira <112106373+NataNogueira@users.noreply.github.com>
2025-10-03 20:09:59 +0000

    fix: corrige forma de download dos arquivos para pegar as pastas corretamente

--- src/download_data.py
+++ src/download_data.py
@@ -2,36 +2,45 @@ import os
 import zipfile
 import shutil
 from kaggle.api.kaggle_api_extended import KaggleApi
-
-# Configurações
-DATASET = "icebearogo/fruit-classification-dataset"
-DEST = "data"
-ZIP_PATH = os.path.join(DEST, "dataset.zip")
-CSV_DIR = os.path.join(DEST, "csv")
-RAW_DIR = os.path.join(DEST, "raw")
+from config import config
 
 # Autentica e baixa o dataset
-os.environ['KAGGLE_CONFIG_DIR'] = os.path.join(os.getcwd(), ".kaggle")
 api = KaggleApi()
+api._load_config({
+    'username':config.KAGGLE_USERNAME,
+    'key':config.KAGGLE_KEY
+})
+ 
 api.authenticate()
-os.makedirs(DEST, exist_ok=True)
-api.dataset_download_files(DATASET, path=DEST, unzip=False)
+os.makedirs(config.DEST, exist_ok=True)
+api.dataset_download_files(config.DATASET, path=config.DEST, unzip=False)
 
 # Extrai para pasta temporária
-TEMP = os.path.join(DEST, "temp")
+TEMP = os.path.join(config.DEST, "temp")
 os.makedirs(TEMP, exist_ok=True)
-with zipfile.ZipFile(ZIP_PATH, 'r') as zip_ref:
+with zipfile.ZipFile(config.ZIP_PATH, 'r') as zip_ref:
     zip_ref.extractall(TEMP)
 
-# Move arquivos para csv/ ou raw/
-for root, _, files in os.walk(TEMP):
-    for f in files:
-        src = os.path.join(root, f)
-        ext = os.path.splitext(f)[1].lower()
-        dest_dir = CSV_DIR if ext == ".csv" else RAW_DIR
-        os.makedirs(dest_dir, exist_ok=True)
-        shutil.move(src, os.path.join(dest_dir, f))
+# Caminho para o dataset extraído
+extracted_folder = os.path.join(TEMP, "Fruit_dataset")
+
+# Cria pastas finais
+os.makedirs(config.RAW_DIR, exist_ok=True)
+os.makedirs(config.CSV_DIR, exist_ok=True)
+
+# Move conteúdo da pasta Fruit_dataset para RAW_DIR
+for item in os.listdir(extracted_folder):
+    src_path = os.path.join(extracted_folder, item)
+   
+    if os.path.isdir(src_path):
+        shutil.move(src_path, os.path.join(config.RAW_DIR, item))
+    else:
+        ext = os.path.splitext(item)[1].lower()
+        if ext == ".csv":
+            shutil.move(src_path, os.path.join(config.CSV_DIR, item))
+        else:
+            shutil.move(src_path, os.path.join(config.RAW_DIR, item))
 
 # Limpeza
-os.remove(ZIP_PATH)
+os.remove(config.ZIP_PATH)
 shutil.rmtree(TEMP)
